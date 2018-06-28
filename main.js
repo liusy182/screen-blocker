8e7caea43992af423f535f5663a8fa392fd73947
liusy182 <liusy182@gmail.com>
2018-06-28 21:09:59 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const {app, BrowserWindow} = require('electron')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({
+    fullscreen: true
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('index.html');
+  win.setVisibleOnAllWorkspaces(true);
+
+  // Open the DevTools.
+  // win.webContents.openDevTools()
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow();
+  }
+
+})
